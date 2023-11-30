5c2eba3010d81574c3b93ce4ca68fde0ac96d39f
Sumit0009 <100469632+sumitkumarsharm@users.noreply.github.com>
2023-11-30 19:43:01 +0000

    All About truthy value

--- 03_Control-Flow/03_Truthy.js
+++ 03_Control-Flow/03_Truthy.js
@@ -1,9 +1,9 @@
-const userEmail = 0
-if (userEmail) {
-    console.log("Got user Email");
-} else {
-    console.log("Don't Get the Email");
-}
+// const userEmail = []
+// if (userEmail) {
+//     console.log("Got user Email");
+// } else {
+//     console.log("Don't Get the Email");
+// }
 
 //Falsy Value
 
@@ -13,6 +13,27 @@ if (userEmail) {
 
 // True, 1, "false", "0", " ", [], {}, function(){}
 
-if (userEmail.length === 0) {
-    console.log("Your Array is Empyt");    
-}
+// if (userEmail.length === 0) {
+//     console.log("Your Array is Empyt");    
+// }
+
+// const EmptyObj = {}
+// if (Object.keys(EmptyObj.length === 0)){
+//     console.log("Object is Empty");
+// }
+
+
+//Nullish Coalescing Operator(??): null Undefind
+
+let vell;
+// vall = 5 ?? 10;
+// vall = null ?? 10
+// vall = undefined ?? 10
+vall = null ?? 10 ?? 12;// it is checking the first value
+// console.log(vall);
+// console.log(vall);
+
+
+// Ternary Operator
+const icePrice = 100
+icePrice<=80? console.log("it is grater than 80"):console.log("Dont buy it is cously");
