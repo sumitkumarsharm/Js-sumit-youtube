e1ee1ff19387d117e140dddacae908745d2bdde9
Sumit0009 <100469632+sumitkumarsharm@users.noreply.github.com>
2023-12-04 17:10:52 +0000

    All About fore in loop

--- 04_Itration/04_For_In_loop.js
+++ 04_Itration/04_For_In_loop.js
@@ -1,58 +1,58 @@
-// const MyObj = {
-//     py:'python',
-//     jv:'java',
-//     cpp:'c++',
-//     js:'javascripts',
-//     Ry:'Ruby'
-// }
-// // here we use for loop on an object 
-// // here i print only key from the object using for in loop
-// for (const key in MyObj) {
-//         // console.log(key);   
-// }
+const MyObj = {
+    py:'python',
+    jv:'java',
+    cpp:'c++',
+    js:'javascripts',
+    Ry:'Ruby'
+}
+// here we use for loop on an object 
+// here i print only key from the object using for in loop
+for (const key in MyObj) {
+        // console.log(key);   
+}
 
-// // here i print only key from the object using for in loop
-// for (const key in MyObj) {
-//     // console.log(MyObj[key]);
-// }
+// here i print only key from the object using for in loop
+for (const key in MyObj) {
+    // console.log(MyObj[key]);
+}
 
-// // here we print value and key both
-// for (const key in MyObj) {
-//     // console.log(`${key} for the ${MyObj[key]}`)
-// }
-
-
-
-// // for in loop using on Array 
-
-
-// // fatching Key on Array
-// const MyArr = ['sumit', 'amit', 'sonu', 'monu', 'dhonu']
-// for (const key in MyArr) {
-//     // console.log(key);
-// }
-
-// //faatching Value form the array
-// for (const key in MyArr) {
-//     // console.log(MyArr[key]);
-// }
-
-// // fatching all value from the array
-// for (const key in MyArr) {
-//     // console.log(`${key} for ${MyArr[key]}`);
-// }
+// here we print value and key both
+for (const key in MyObj) {
+    // console.log(`${key} for the ${MyObj[key]}`)
+}
 
 
 
-// // map is not ittratable 
-// const map = new Map();
-// map.set('roll1','shani')
-// map.set('roll12','sumit')
-// map.set('roll3','Manish')
-// map.set('roll4','Amit')
+// for in loop using on Array 
 
-// for (const key in map) {
-//         console.log(key);
-// }
+
+// fatching Key on Array
+const MyArr = ['sumit', 'amit', 'sonu', 'monu', 'dhonu']
+for (const key in MyArr) {
+    // console.log(key);
+}
+
+//faatching Value form the array
+for (const key in MyArr) {
+    // console.log(MyArr[key]);
+}
+
+// fatching all value from the array
+for (const key in MyArr) {
+    // console.log(`${key} for ${MyArr[key]}`);
+}
+
+
+
+// map is not ittratable 
+const map = new Map();
+map.set('roll1','shani')
+map.set('roll12','sumit')
+map.set('roll3','Manish')
+map.set('roll4','Amit')
+
+for (const key in map) {
+        console.log(key);
+}
 
 
