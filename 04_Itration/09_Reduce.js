1751aa38ca122265413024a1e8da137aef92b6db
Sumit0009 <100469632+sumitkumarsharm@users.noreply.github.com>
2023-12-05 18:37:38 +0000

    All About Reduce in js

--- 04_Itration/09_Reduce.js
+++ 04_Itration/09_Reduce.js
@@ -1,14 +1,47 @@
 // Reduce function
 const  SNumber = [1,2,3]
 let newnumber = SNumber.reduce(function(acc,currval){
-    console.log(`acc:${acc} and currval ${currval}`);
+    // console.log(`acc:${acc} and currval ${currval}`);
     return acc+currval
 },0)
-console.log(newnumber);
+// console.log(newnumber);
+
 
 //arrow function
 let bignumber = SNumber.reduce((acc,currval)=>{
-    console.log(`acc:${acc} and currval ${currval}`);
+    // console.log(`acc:${acc} and currval ${currval}`);
     return acc+currval
-},0)
-console.log(bignumber);
+},3)
+// console.log(bignumber);
+
+// how to write it in arrow function
+const mytotal =SNumber.reduce((acc, curr)=> acc+curr, 3)
+console.log(mytotal);
+
+
+// using reduce on Object
+const shoppingCart = [
+    {
+        itemName:"JavaScripts",
+        price:12000
+    },
+    {
+        itemName:"Java",
+        price:990
+    },
+    {
+        itemName:"Web Development",
+        price:1200000
+    },
+    {
+        itemName:"React",
+        price:4590
+    },
+    {
+        itemName:"Angular",
+        price:9892
+    }
+]
+const newShooping  = shoppingCart.reduce((acc,item)=>acc + item.price, 0)
+
+console.log(newShooping);
